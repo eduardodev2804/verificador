15d6ae5900a482d2fe909f4a9e83c80ada071945
eduardodev2804 <132065352+eduardodev2804@users.noreply.github.com>
2024-10-22 09:40:38 -0300

    Add files via upload

---
+++ .js
@@ -0,0 +1,10 @@
+function verificarIdade(){
+
+const idade = document.getElementById
+('Idade').value;
+const resultado = document.getElementById('resultado')
+
+{if(idade >= 17)
+    resultado.textContent = " Voçê é maior de idade"
+}
+}
